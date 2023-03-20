5cb4dd61d970733008bcc39b007944b90aadddc4
Mircolo <bismirco@gmail.com>
2023-03-20 17:05:11 +0100

    updating

--- index.js
+++ index.js
@@ -1,46 +1,26 @@
-const bubbleDiv = document.querySelector(".bubble") ;
 var winWidth = window.innerWidth;
 var winHeight = window.innerHeight;
 var bubbles = document.getElementsByClassName('bubble');
 
+  
+window.initMap = initMap();
+
+
+
+
+// ======================================= FUNCTIONS ==========================================
 // Initialize and add the map
 function initMap() {
-    // The location of Uluru
-    const uluru = { lat: 45.850755, lng: 11.077408 };
-    // The map, centered at Uluru
-    const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 18,
-      center: uluru,
-    });
-    // The marker, positioned at Uluru
-    const marker = new google.maps.Marker({
-      position: uluru,
-      map: map,
-    });
-  }
-  
-  window.initMap = initMap;
-
-
-
-console.log(bubbles.length)
-
-for ( var i=0; i < bubbless.length; i++ ) {
- 	console.log(thisDiv)
-    // shortcut! the current div in the list
-    var thisDiv = bubbles[i];
-    
-    // get random numbers for each element
-    randomTop = getRandomNumber(0, winHeight);
-    randomLeft = getRandomNumber(0, winWidth);
-    
-    // update top and left position
-    thisDiv.style.top = randomTop +"px";
-    thisDiv.style.left = randomLeft +"px";
-    
-}
-
-// function that returns a random number between a min and max
-function getRandomNumber(min, max) {
-  return Math.random() * (max - min) + min;
+  // The location of Uluru
+  const uluru = { lat: 45.850755, lng: 11.077408 };
+  // The map, centered at Uluru
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 15,
+    center: uluru,
+  });
+  // The marker, positioned at Uluru
+  const marker = new google.maps.Marker({
+    position: uluru,
+    map: map,
+  });
 }
