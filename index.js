4f9be836c06b8e828b330f6cf84fad12ffec9a80
Mircolo <bismirco@gmail.com>
2023-03-21 17:05:37 +0100

    resetted project

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 var winWidth = window.innerWidth;
 var winHeight = window.innerHeight;
 var bubbles = document.getElementsByClassName('bubble');
-
+var webcam = document.querySelector("#webcam")
 
 window.initMap = initMap();
+//document.querySelector("#webcam").style.transform = "scale(1, 0.1)";
 
 // ======================================= FUNCTIONS ==========================================
 // Initialize and add the map
@@ -21,3 +22,15 @@ function initMap() {
     map: map,
   });
 }
+
+/* function onResize(event){
+var w = window.innerWidth;
+var h = window.innerHeight;
+
+
+
+webcam.style.transform = "scale("+ w/1400 + "," + w/1400 + ") "// + "translateX("+  w/100 + "px)"
+console.log(w, h, webcam.getBoundingClientRect().x, webcam.getBoundingClientRect().y)
+//webcam.style.transform = "translateX("+  w/100 + "px)";
+}  */
+
